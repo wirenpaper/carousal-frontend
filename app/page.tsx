379f04dfb09b8181535739dd2159b9ae109fcdf9
wnp <chumchumchira@gmail.com>
2024-06-14 17:44:01 +0500

    ui is set, onclicks figured, now writing hooks

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,49 @@
-export default function Home() {
+"use client";
+import React, { useState } from 'react';
+
+const images = [
+  "/images/img1.jpeg",
+  "/images/img2.jpeg",
+  "/images/img3.jpeg",
+  "/images/img4.jpeg",
+  "/images/img5.jpeg",
+];
+
+const Home: React.FC = () => {
+  const prev_img = () => {
+    console.log("prev_img");
+  };
+
+  const next_img = () => {
+    console.log("next_img");
+  };
+
   return (
     <div>
       <h1 className="text-center p-8">Project 1: Carousel</h1>
       <div className="flex justify-between px-16">
         <div className="flex items-center">
-          <div className="flex justify-center bg-black p-2 rounded-full w-10 text-white">
+          <button
+            className="flex justify-center bg-black p-2 rounded-full w-10 text-white"
+            onClick={prev_img}
+          >
             ⬅
-          </div>
+          </button>
         </div>
         <div className="bg-red-100 w-8/12 flex justify-center">
-          <img className="h-96" src="https://images.pexels.com/photos/2792157/pexels-photo-2792157.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"></img>
+          <img className="h-120" src="/images/img4.jpeg"></img>
         </div>
         <div className="flex items-center">
-          <div className="flex justify-center bg-black p-2 rounded-full w-10 text-white">
+          <button
+            className="flex justify-center bg-black p-2 rounded-full w-10 text-white"
+            onClick={next_img}
+          >
             ⮕
-          </div>
+          </button>
         </div>
       </div>
     </div>
   );
 }
+
+export default Home;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      height: {
+        '120': '35rem',
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
